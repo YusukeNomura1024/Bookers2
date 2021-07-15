5e8477804f085bb64a626149a7f0316b3b2b5606
YusukeNomura1024 <joblesser@gmail.com>
2021-07-15 15:30:23 +0000

    [Fix] books column

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
   get 'home/about' => 'homes#about'
   devise_for :users
   resources :books
   root to: 'homes#top'
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_125404) do
+ActiveRecord::Schema.define(version: 2021_07_15_152025) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "text"
-    t.string "body"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
