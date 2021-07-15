6c6bd36b6e83486cc364015bbe0ab157c7fda302
YusukeNomura1024 <joblesser@gmail.com>
2021-07-15 14:35:25 +0000

    [Add] booksindex right

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,19 @@
 class BooksController < ApplicationController
   def index
     @user = User.find(current_user.id)
+    @books = Book.all
 
   end
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
