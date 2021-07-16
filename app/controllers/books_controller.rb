5cf796945ebe7160b86e79b052aab46731297a83
YusukeNomura1024 <joblesser@gmail.com>
2021-07-16 04:23:20 +0000

    [Fix] BooksController create @book.user_id = current_user.id

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,15 @@ class BooksController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @books = Book.all
+    @book = Book.new
 
   end
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(@book[:id])
+    redirect_to book_path(@book.id)
+
   end
 
   private
