a5840d4b80f07156340dfb8671e36919fb947f0b
YusukeNomura1024 <joblesser@gmail.com>
2021-07-16 10:51:21 +0000

    [Add] 新規投稿が失敗したとき

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+
   end
 
 
@@ -15,6 +16,22 @@ class UsersController < ApplicationController
     @users = User.all
     @user = User.find(current_user.id)
     @book = Book.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7c62acdd60734fd745ea1508e18a181535477cd7859746defdedf150bab316ac5f16a34c67b16be4cfe628b198001a715c18b213c3353139cceea108a6622380'
