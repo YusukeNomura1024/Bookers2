efc6bc26845de3af9b02d1b53f73a07cd5efc991
YusukeNomura1024 <joblesser@gmail.com>
2021-07-16 08:04:02 +0000

    [Add] userのeditページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
   def index
     @users = User.all
     @user = User.find(current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   root to: 'homes#top'
-  resources :users, only: [:show, :index, ]
+  resources :users, only: [:show, :index, :edit ]
 end
